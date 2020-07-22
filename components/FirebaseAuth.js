f968faefcfa01d828ef6f04cfcef3bffb7ff3d5f
jarrodwatts <35651410+jarrodwatts@users.noreply.github.com>
2020-07-22 21:11:32 +1000

    Add Facebook and Google Auth

--- components/FirebaseAuth.js
+++ components/FirebaseAuth.js
@@ -11,13 +11,38 @@ initFirebase()
 
 const firebaseAuthConfig = {
   signInFlow: 'popup',
-  // Auth providers
+  // Auth providers  
   // https://github.com/firebase/firebaseui-web#configure-oauth-providers
   signInOptions: [
     {
       provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
       requireDisplayName: false,
     },
+    {
+      provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      scopes: [
+        'https://www.googleapis.com/auth/contacts.readonly'
+      ],
+      customParameters: {
+        // Forces account selection even when one account
+        // is available.
+        prompt: 'select_account'
+      }
+    },
+    {
+      provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      scopes: [
+        'public_profile',
+        'email',
+        'user_likes',
+        'user_friends'
+      ],
+      customParameters: {
+        // Forces password re-entry.
+        auth_type: 'reauthenticate'
+      }
+    },
+
   ],
   signInSuccessUrl: '/',
   credentialHelper: 'none',
