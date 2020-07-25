917cf4397705fd1e23750c079a54eb37bd60e720
jarrodwatts <35651410+jarrodwatts@users.noreply.github.com>
2020-07-25 17:43:18 +1000

    Try fix build errors by using dynamic imports

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,10 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import NavBar from '../components/Header';
-import Footer from '../Components/Footer';
+import dynamic from 'next/dynamic'
+
+const Footer = dynamic(() => import('../Components/Footer'))
+const NavBar = dynamic(() => import('../components/Header'))
 
 const useStyles = makeStyles((theme) => ({
   icon: {
